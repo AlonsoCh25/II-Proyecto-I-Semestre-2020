2801094a0551d3aca63e2193285571d7ac621ed9
Kenneth C <kennethalonsoc@gmail.com>
2020-07-12 21:39:17 -0600

    Ventana de login
    Manejo de la ventana de login, se implementa la creacion de usuarios o cargar anteriores usuarios de manera automatica.

--- MAIN.py
+++ MAIN.py
@@ -5,19 +5,35 @@
 #Kenneth Castillo 2019062984
 """Libraries"""
 from CLASSES import *
-import pygame
+from pygame import *
 
 """Functions"""
-def principal_window():
+def principal_window(row):
     #Settings of the screen
     pygame.init()
     weight, height = 952,768
     screen = pygame.display.set_mode((weight,height))
     
+    #CSV Archive
+    csv_scoreboard = csv_class("ScoreBoard.csv","rt")
+
+    #Matrix
+    matrix = csv_scoreboard.get_matrix()
+    
     #Sttings of the bottons
     bt_weight,bt_heigth = 200,150
 
+    #Fonts
+    font_user = pygame.font.Font("triforce.ttf",40)
+
+    #Text
+    user_text = matrix[row][0] 
+    scoreboard_text = "ScoreBoard:" +" "+ matrix[row][1]
     
+    #Render
+    txt_user = font_user.render(user_text, True, (255,255,255))
+    txt_scoreboard = font_user.render(scoreboard_text, True, (255,255,255))
+
     #Set initial clock
     clock = pygame.time.Clock()
     
@@ -66,11 +82,15 @@ def principal_window():
         bt_play.update(screen,cursor)
         bt_scoreboard.update(screen,cursor)
         cursor.update()
-
+        screen.blit(txt_user,(0,0))
+        screen.blit(txt_scoreboard,(0,40))
+        
         pygame.display.update()
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                csv_scoreboard.write(matrix)
+                csv_scoreboard.update_matrix("ScoreBoard.csv","w")
                 exit_ = True
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if cursor.colliderect(bt_credits.rect):
@@ -81,6 +101,8 @@ def principal_window():
                     break
                 if cursor.colliderect(bt_exit.rect):
                     print("push_exit")
+                    csv_scoreboard.write(matrix)
+                    csv_scoreboard.update_matrix("ScoreBoard.csv","w")
                     exit_ = True
                 if cursor.colliderect(bt_help.rect):
                     print("Push Help")
@@ -88,8 +110,7 @@ def principal_window():
                     exit_ = True
                     pygame.quit()
                     break
-                if cursor.colliderect(bt_login.rect):
-                    print("Push Login")
+                
                 if cursor.colliderect(bt_play.rect):
                     print("Push Play")
                 if cursor.colliderect(bt_scoreboard.rect):
@@ -143,6 +164,115 @@ def credits_window():
         pygame.display.update()
         clock.tick(60)
     pygame.quit()
+
+def login_window():
+    #import pygame_textinput
+    #Settings of the screen
+    pygame.init()
+    pygame.font.init
+    weight, height = 400,300
+    login_screen = pygame.display.set_mode((weight,height))
+    
+    
+    
+    #FONT
+    font = pygame.font.Font(None,30)
+    font_user = pygame.font.Font("triforce.ttf",40)
+    
+    #Text input
+    box_input = pygame.Rect(100, 110, 140, 32)
+
+    #Color of the box
+    color_inactive = (0,0,0)
+    color_active = (255,255,255)
+    color = color_inactive
+
+    #Set the initial active of the box
+    active = False
+
+    #Content the text of the user
+    text = ''
+    User = "User Name"
+
+    #render the elements
+    txt_user = font_user.render(User, True, (0,0,0))
+    user_rect = txt_user.get_rect()
+    
+    #Sttings of the bottons
+    bt_weight,bt_heigth = 200,150
+    
+    #Set initial clock
+    clock = pygame.time.Clock()
+    
+    #Images of the screen
+    background = pygame.image.load("rsc/background.jpeg")
+    img_login = pygame.image.load("rsc/btn_login.png")
+    img_login_b = pygame.image.load("rsc/btn_login_b.png")
+    
+    
+    
+    #Buttons of the screen
+    cursor = Cursor()
+    bt_login = Button(img_login,img_login_b,(weight/2-(bt_weight/2)),(weight/3),bt_weight,bt_heigth)
+
+    
+    pygame.display.update()
+    #While of the loop
+    exit_ = False
+    while exit_ != True:
+        #Background
+        login_screen.blit(pygame.transform.scale(background,(weight,height)),(0,0))
+
+        #render the elements
+        txt = font.render(text, True, (0,0,0))
+        
+        #Scale the boxof the text
+        width = max(100, txt.get_width()+10)
+        box_input.w = width
+        box_input.x = round((weight//2)-(width//2))
+        
+                                
+        
+        
+        #Set the blits in the screen
+        login_screen.blit(txt, (box_input.x+5, box_input.y+5))
+        pygame.draw.rect(login_screen, color, box_input, 2)
+        bt_login.update(login_screen,cursor)
+        login_screen.blit(txt_user,(round(((weight/2)-(user_rect.w/2))),60))
+        
+        cursor.update()
+
+        
+        pygame.display.update()
+        clock.tick(60)
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit_ = True
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if cursor.colliderect(bt_login.rect):
+                    transition_login(text)
+                    print("Push Login")
+                    exit_ = True
+                    
+                # when the user click in the box, this is active
+                if box_input.collidepoint(event.pos):
+                    # Set the value of the variable
+                    active = not active
+                else:
+                    active = False
+                #Set the current color of the box
+                color = color_active if active else color_inactive
+            if event.type == pygame.KEYDOWN:
+                if active:
+                    if event.key == pygame.K_RETURN:
+                        text = ''
+                    elif event.key == pygame.K_BACKSPACE:
+                        text = text[:-1]
+                    else:
+                        text += event.unicode
+        
+    pygame.quit()
     
 def help_window():
     #Settings of the screen
@@ -184,6 +314,30 @@ def help_window():
 
     pygame.quit()
 
+def transition_login(user):
+    csv_scoreboard = csv_class("ScoreBoard.csv","rt")
+    matrix = csv_scoreboard.get_matrix()
+    
+    initial_scoreboard = "0"
+    found = False
+    row = 0
+    if matrix != []:
+        for usr in matrix:
+            if usr[0] == user:
+                found = True
+                principal_window(matrix,row)
+            else:
+                row += 1
+        if not found:     
+            matrix.append((user, initial_scoreboard))
+            csv_scoreboard.write(matrix)
+            csv_scoreboard.update_matrix("ScoreBoard.csv","w")
+            principal_window(row)
+    else:
+        csv_scoreboard.write(matrix)
+        csv_scoreboard.update_matrix("ScoreBoard.csv","w")
+        matrix.append([user, initial_scoreboard])
+        principal_window(row)
 
 def multi_line_reader(screen, txt, x,y, font,colour=(128,128,128), justification="left"):
     def update():
@@ -210,6 +364,6 @@ def multi_line_reader(screen, txt, x,y, font,colour=(128,128,128), justification
         update()
         y += bitmap.get_height()
 
-principal_window()
+login_window()
     
     

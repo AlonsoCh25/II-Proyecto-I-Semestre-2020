97dddcc49ce4a538f4a9d1ae7ef54337e16e6896
Quigonar <67380073+Quigonar@users.noreply.github.com>
2020-07-17 23:34:29 -0600

    Clases avatars
    Cambio de las clases de avatars

---
+++ Archer.py
@@ -0,0 +1,68 @@
+import pygame
+
+arrows = pygame.sprite.Group()
+FPS = 30
+
+"""__________________________________________________________________________________"""
+
+class Archer(pygame.sprite.Sprite):
+
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('images/avatars/archer1.png')
+        self.image = pygame.transform.scale(self.image, (95, 80))
+        self.rect = self.image.get_rect()
+        self.attack_time = 10
+        self.movement_time = 12
+        self.health = 5
+        self.damage = 2
+        self.rect.left = x
+        self.rect.top = y
+        self.counter_attack = 0
+        self.counter_move = 0
+        self.change_counter = 0
+        self.y_pos = 0
+
+    def update(self, superficie, gridMatrix):
+
+        self.counter_attack += 1
+
+        if self.counter_attack >= self.attack_time * FPS:
+            change_pos = 10
+            self.image = pygame.image.load('images/avatars/archer2.png')
+            self.image = pygame.transform.scale(self.image, (95, 80))
+            self.change_counter += 1
+            if self.change_counter >= change_pos:
+                arrow = Arrow(self.rect.centerx, self.rect.bottom)
+                arrows.add(arrow)
+                self.counter_attack = 0
+                self.image = pygame.image.load('images/avatars/archer1.png')
+                self.image = pygame.transform.scale(self.image, (95, 80))
+                self.change_counter = 0
+
+        self.counter_move += 1
+
+        if self.counter_move >= self.movement_time * FPS:
+            self.rect.top += 80
+            self.counter_move = 0
+
+        superficie.blit(self.image, self.rect)
+
+"""__________________________________________________________________________________"""
+
+class Arrow(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("images/avatars/arrow.png")
+        self.image = pygame.transform.rotate(self.image, 90)
+        self.image = pygame.transform.scale(self.image, (50,50))
+        self.rect = self.image.get_rect()
+        self.rect.bottom = y
+        self.rect.centerx = x
+        self.speedy = 10
+
+    def update(self, superficie):
+        self.rect.y += self.speedy
+        if self.rect.top > 1000:
+            self.kill()
+        superficie.blit(self.image, self.rect)
---
+++ Squire.py
@@ -0,0 +1,67 @@
+import pygame
+
+swords = pygame.sprite.Group()
+FPS = 30
+
+"""__________________________________________________________________________________"""
+
+class Squire(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('images/avatars/squire1.png')
+        self.image = pygame.transform.scale(self.image, (95, 80))
+        self.rect = self.image.get_rect()
+        self.attack_time = 15
+        self.movement_time = 10
+        self.health = 10
+        self.damage = 3
+        self.spawn = False
+        self.rect.left = x
+        self.rect.top = y
+        self.counter_attack = 0
+        self.counter_move = 0
+        self.change_counter = 0
+
+    def update(self, superficie, gridMatrix):
+
+        self.counter_attack += 1
+
+        if self.counter_attack >= self.attack_time * FPS:
+            change_pos = 10
+            self.image = pygame.image.load('images/avatars/squire2.png')
+            self.image = pygame.transform.scale(self.image, (95, 80))
+            self.change_counter += 1
+            if self.change_counter >= change_pos:
+                sword = Sword(self.rect.centerx, self.rect.bottom)
+                swords.add(sword)
+                self.counter_attack = 0
+                self.image = pygame.image.load('images/avatars/squire1.png')
+                self.image = pygame.transform.scale(self.image, (95, 80))
+                self.change_counter = 0
+
+        self.counter_move += 1
+
+        if self.counter_move >= self.movement_time * FPS:
+            self.rect.top += 80
+            self.counter_move = 0
+
+        superficie.blit(self.image, self.rect)
+
+"""__________________________________________________________________________________"""
+
+class Sword(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("images/avatars/sword.png")
+        self.image = pygame.transform.rotate(self.image, 270)
+        self.image = pygame.transform.scale(self.image, (50, 50))
+        self.rect = self.image.get_rect()
+        self.rect.bottom = y
+        self.rect.centerx = x
+        self.speedy = 10
+
+    def update(self, superficie):
+        self.rect.y += self.speedy
+        if self.rect.top > 1000:
+            self.kill()
+        superficie.blit(self.image, self.rect)
